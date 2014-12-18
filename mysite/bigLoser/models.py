a7c37381d8d76f1f131e0178921e3e93d5fd1579
Tom Hall <tom.hall@versionone.com>
2014-12-17 20:57:17 -0500

    model changes

--- mysite/bigLoser/models.py
+++ mysite/bigLoser/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Weight(models.Model):
+    current_weight = models.IntegerField(default=0)
+    current_date = models.DateTimeField('date published')
+
+
