0c8bbc1d7996c4fd0b9db1baf25bb032d41c9d4d
Tom Hall <tom.hall@versionone.com>
2014-12-17 21:21:55 -0500

    added self str

--- mysite/bigLoser/models.py
+++ mysite/bigLoser/models.py
@@ -6,5 +6,7 @@ from django.db import models
 class Weight(models.Model):
     current_weight = models.IntegerField(default=0)
     current_date = models.DateTimeField('date published')
+    def __str__(self):              # __unicode__ on Python 2
+        return self.current_date.strftime('%m/%d/%Y')+ ' ' + str(self.current_weight)
 
 
