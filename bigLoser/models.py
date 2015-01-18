2e57df3ab3faf60a14b3a2d634856db4497d7137
Tom Hall <tom.hall@versionone.com>
2015-01-18 12:35:31 -0500

    refactoring models

--- bigLoser/models.py
+++ bigLoser/models.py
@@ -2,25 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
 from django.conf import settings
-from django.db import models
 from gcharts import GChartsManager
-
-# Create your models here.
-
-def gen_default_contestant():
-	userList=User.objects.all()
-	if len(userList) > 0: return userList[0].id
-	return None
-
-class Weight(models.Model):
-    objects = GChartsManager()
-    current_weight = models.IntegerField(default=0)
-    current_date = models.DateTimeField('date published', default=datetime.now)
-    contestant = models.ForeignKey(settings.AUTH_USER_MODEL,
-    	unique_for_date="current_date",
-    	default=gen_default_contestant)
-    def __str__(self):              # __unicode__ on Python 2
-        return self.current_date.strftime('%m/%d/%Y')+ ' ' + str(self.current_weight)
 
 class Contest(models.Model):
     contest_name = models.CharField(max_length=50)
@@ -34,7 +16,26 @@ class Contestant(models.Model):
     contest = models.ForeignKey(Contest)
     target_weight = models.IntegerField(default=0)
     def __str__(self):              # __unicode__ on Python 2
-        return "%s %s" % (self.user, self.contest)
+        return "%s in %s contest" % (self.user, self.contest)
+
+def gen_default_contestant():
+        contestantList=Contestant.objects.all()
+        if len(contestantList) > 0: return contestantList[0].id
+        return None
+
+class Weight(models.Model):    
+    objects = GChartsManager()
+    current_weight = models.IntegerField(default=0)
+    current_date = models.DateTimeField(default=datetime.now)
+    contestant = models.ForeignKey(Contestant,
+    	unique_for_date="current_date",
+    	default=gen_default_contestant)
+    def __str__(self):              # __unicode__ on Python 2
+        return str(self.contestant.user) + ' weighed ' + str(self.current_weight) + ' on ' + self.current_date.strftime('%m/%d/%Y') 
+
+
+
+
 
 
 
