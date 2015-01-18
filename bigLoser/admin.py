c70698ed920c7ea3de371380ce23ce7188439b5f
Tom Hall <tom.hall@versionone.com>
2015-01-18 11:27:47 -0500

    add contest to admin page

--- bigLoser/admin.py
+++ bigLoser/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from bigLoser.models import Weight
+from bigLoser.models import Weight, Contest, Contestant
 
 # Register your models here.
 
 admin.site.register(Weight)
+admin.site.register(Contest)
+admin.site.register(Contestant)
+
