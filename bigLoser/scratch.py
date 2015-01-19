d6491adf7299dfbeb941c5c6dadb062ef9e66a8f
Tom Hall <tom.hall@versionone.com>
2015-01-19 17:03:35 -0500

    delete scratch

--- bigLoser/scratch.py
+++
@@ -1,8 +0,0 @@
-url(r'^accounts/register/$', 'django_test.views.register_user'),
-url(r'^accounts/register_success/$', 'django_test.views.register_success'),
-
-from django.contrib.auth.forms import UserCreationForm
-
-def register_user(request):
-	if request.method == 'POST':
-		form = UserCreationForm(request.POST)
