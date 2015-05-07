63291648ec4089ed53cda946ff7bd3d88ee797cf
Tom Hall <tom.hall@versionone.com>
2015-05-07 08:55:06 -0400

    recreate migrations for psql

--- bigLoser/migrations/0001_initial.py
+++ bigLoser/migrations/0001_initial.py
@@ -2,20 +2,49 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
+import bigLoser.models
+import datetime
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
+        migrations.CreateModel(
+            name='Contest',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+                ('start_date', models.DateField()),
+                ('end_date', models.DateField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Contestant',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('target_weight', models.IntegerField(default=0)),
+                ('contest', models.ForeignKey(to='bigLoser.Contest')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
         migrations.CreateModel(
             name='Weight',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('current_weight', models.IntegerField(default=0)),
-                ('current_date', models.DateTimeField(verbose_name=b'date published')),
+                ('current_date', models.DateField(default=datetime.datetime.now)),
+                ('contestant', models.ForeignKey(default=bigLoser.models.gen_default_contestant, to='bigLoser.Contestant', unique_for_date=b'current_date')),
             ],
             options={
             },
